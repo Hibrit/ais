c8061d5f49f4eb1c5da626d7827fdaaeee20800a
hibrit <taskin.n.dogan@yandex.com>
2021-07-13 22:16:22 +0300

    renamed files

--- 2.sh
+++ 2.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-#* setting time
-ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
-hwclock --systohc
+#* installing the essential packages
+pacstrap /mnt base linux linux-firmware git vim 
+# intel-ucode
 
-sed -i '177s/.//' /etc/locale.gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "machinename" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 machinename.localdomain machinename" >> /etc/hosts
-echo root:123 | chpasswd
+#* generating fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+echo
+echo 'filesystem table created'
+echo
+sleep 3
 
-#! create a user and set its wheel permissions
+#? clone this repo inside /mnt/root
+git clone https://github.com/Hibrit/arch_install_scripts.git /mnt/
 
-pacman -S --needed --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils bash-completion openssh reflector firewalld os-prober 
-# nvidia nvidia-utils nvidia-settings avahi virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft bluez bluez-utils cups tlp gvfs gvfs-smb nfs-utils inetutils dnsutils hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol sof-firmware nss-mdns acpid terminus-font rsync acpi acpi_call ipset ntfs-3g 
-
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-# systemctl enable bluetooth
-# systemctl enable cups.service
-systemctl enable sshd
-# systemctl enable avagi-daemon
-# systemctl enable tlp
+#* chroot
+arch-chroot /mnt
