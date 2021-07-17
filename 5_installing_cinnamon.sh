0e416efe9ddb6fb83b540ad14877e4b53de7cabf
hibrit <taskin.n.dogan@yandex.com>
2021-07-17 05:36:22 +0300

    .

--- 5_installing_cinnamon.sh
+++ 5_installing_cinnamon.sh
@@ -8,7 +8,9 @@ ${HOME}/Documents/setupscripts/install_yay.sh
 
 sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan numlockx cinnamon alacritty firefox papirus-icon-theme ttf-input ttf-ubuntu-font-family ttf-fira-code mlocate neofetch htop
 
-yay -Sy --needed --noconfirm papirus-folders bibata-cursor-theme-bin nerd-fonts-meslo
+yay -Sy --needed --noconfirm mint-themes papirus-folders bibata-cursor-theme-bin nerd-fonts-meslo
+
+papirus-folders -C yaru -t Papirus-Dark
 
 sudo rm /etc/lightdm/lightdm.conf /etc/lightdm/lightdm-webkit2-greeter.conf
 
