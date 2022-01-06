677b9641caf7f43b4d1aab8b36ab8da897146841
hibrit <taskin.n.dogan@yandex.com>
2022-01-06 16:49:23 +0300

    a litte add on to packages

--- 7_terminal_setup.sh
+++ 7_terminal_setup.sh
@@ -13,7 +13,7 @@ echo
 #* essentials
 echo 'Installing some essential packages through yay and pacman'
 sudo pacman -Sy --needed --noconfirm zsh fzf thefuck
-yay -Sy --needed --noconfirm ruby-colorls
+yay -Sy --needed --noconfirm ruby-colorls ookla-speedtest-bin
 
 
 #* oh my zsh
