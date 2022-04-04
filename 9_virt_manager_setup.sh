8b75f5a94a66a29da6107b32d062927c0ce10ba9
hibrit <taskin.n.dogan@yandex.com>
2022-04-04 20:26:55 +0300

    little tweaks with comments

--- 9_virt_manager_setup.sh
+++ 9_virt_manager_setup.sh
@@ -7,19 +7,17 @@ clear
 sudo pacman -Syy --noconfirm archlinux-keyring
 
 
-#? install the needed packages
+#* install packages
 sudo pacman -Sy qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat iptables-nft libguestfs edk2-ovmf
 
-#? edit the conf file '/etc/libvirt/libvirtd.conf'
+#* edit the conf file '/etc/libvirt/libvirtd.conf'
 sudo sed -e '/unix_sock_group/s/^#//' -e '/unix_sock_rw_perms/s/^#//' -i /etc/libvirt/libvirtd.conf
 
-#? configure qemu in case of permission problems
+#* configure qemu in case of permission problems
 sudo sed -e "/#user = \".*\"/s/^#//" -e "/user = \".*\"/s/\".*\"/\"$USER\"/" -e "/#group = \".*\"/s/^#//" -e '/group = ".*"/s/".*"/"libvirt"/' -i /etc/libvirt/qemu.conf
 
-#? add user to group
-#! really have no idea if this works
+#* add user to group
 sudo usermod -a -G libvirt $(whoami)
 
-#? start and enable libvirtd.service
-#! is this really neccessary if we restart it later on instead couldn't we start this service later on
+#* start and enable libvirtd.service
 sudo systemctl enable --now libvirtd.service
