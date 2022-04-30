95780b7374594910bec0246d85e93df68fbf0eed
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 02:43:49 +0300

    added the setup_hibrit

---
+++ 0_setup_hibrit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# author: hibrit
+# purpose: this script will install arch linux by itself
+
+#! even after done with live-iso set a automatic one time script to install the rest of system on first boot
