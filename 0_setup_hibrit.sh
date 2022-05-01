838114b06e343f57c727525351eb8525bfcb6e56
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 03:21:57 +0300

    .

--- 0_setup_hibrit.sh
+++ 0_setup_hibrit.sh
@@ -3,8 +3,10 @@
 # purpose: this script will install arch linux by itself
 
 #! even after done with live-iso set a automatic one time script to install the rest of system on first boot
+#! on first boot set auto log-in and immidiately after logging in deisable that
 #! definitely install both for uefi and legacy than autoselect it
 #! lets use python scripts, dialog package (it is not default for arch live-iso)
+#! maybe curses can be used with the pre-developed idea of tui in passman project
 #! to inform and communucate with user
 #! at the end transform this to a package that can be installed to arch linux
 #! THIS IS BASICALLY A TUI INSTALLER TO ARCH LINUX ^o^
