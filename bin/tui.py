71b003235f6b91780e02839efbece039038f96dd
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 05:42:40 +0300

    .

--- bin/tui.py
+++ bin/tui.py
@@ -7,7 +7,7 @@ except ModuleNotFoundError:
     from sys import executable
     from subprocess import call, STDOUT
     call(f'{executable} -m pip install pythondialog'.split())
-    if 'no module' in STDOUT.lower():
+    if STDOUT == -2:
         call('pacman -Sy --noconfirm --needed  python-pip'.split())
     call(f'{executable} -m pip install pythondialog'.split())
     from dialog import Dialog
