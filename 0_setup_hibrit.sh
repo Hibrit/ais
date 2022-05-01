ffb520ecdd96c5a89d85252b8e7afbb46d389e31
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 03:14:25 +0300

    more comments

--- 0_setup_hibrit.sh
+++ 0_setup_hibrit.sh
@@ -3,3 +3,11 @@
 # purpose: this script will install arch linux by itself
 
 #! even after done with live-iso set a automatic one time script to install the rest of system on first boot
+#! definitely install both for uefi and legacy than autoselect it
+#! lets use python scripts, dialog package (it is not default for arch live-iso)
+#! to inform and communucate with user
+#! at the end transform this to a package that can be installed to arch linux
+#! THIS IS BASICALLY A TUI INSTALLER TO ARCH LINUX ^o^
+#! most automatic version of it will set up my own things entirely
+#! this can be achieved with a config file and let anybody create their owns
+#! so arch linux installations can be shared this way .....
