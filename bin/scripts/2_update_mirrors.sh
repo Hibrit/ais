318251ed61d9fed52f0b6d8d21d7f7f423d50d78
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 07:28:26 +0300

    .

--- bin/scripts/2_update_mirrors.sh
+++ bin/scripts/2_update_mirrors.sh
@@ -7,17 +7,17 @@ source "$PATH/global.sh"
 
 
 #* check for wget first
+log_start 'ARCHLINUX-KEYRING'
+/usr/bin/pacman -Sy --noconfirm archlinux-keyring 1>> /var/log/ais.out 2>> /var/log/ais.err
 log_start 'WGET'
 /usr/bin/pacman -Sy --noconfirm --needed wget 1>> /var/log/ais.out 2>> /var/log/ais.err
 
-sleep 3
-
 #* set the proper pacman configuration
 log_start 'PACMAN CONFIG'
-/usr/bin/rm /etc/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
+# /usr/bin/rm /etc/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
 
 
 #? before this we need to install wget
-/usr/bin/wget --directory-prefix /etc/ https://raw.githubusercontent.com/Hibrit/archlinuxsettings/master/pacman/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
+# /usr/bin/wget --directory-prefix /etc/ https://raw.githubusercontent.com/Hibrit/archlinuxsettings/master/pacman/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
 
-/usr/bin/pacman -Syy 1>> /var/log/ais.out 2>> /var/log/ais.err
+# # /usr/bin/pacman -Syy 1>> /var/log/ais.out 2>> /var/log/ais.err
