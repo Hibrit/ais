83404ffae71fe4f591ea14a1f2d55e1772f18fbd
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 08:32:31 +0300

    .

--- bin/scripts/2_update_mirrors.sh
+++ bin/scripts/2_update_mirrors.sh
@@ -3,11 +3,11 @@
 local_path=$(dirname -- "$(readlink -f "${BASH_SOURCE}")")
 source "$local_path/global.sh"
 
-#! remember to generate optimal repositories with reflector
 
 #* check for wget first
 log_start 'update mirrors'
-reflector --country '' --protocol https --latest 5 --save /etc/pacman.d/mirrorlist
+#? generate optimal repositories with reflector
+#? reflector --country '' --protocol https --latest 5 --save /etc/pacman.d/mirrorlist
 pacman -Syy --noconfirm --needed wget 1>> /var/log/ais.out 2>> /var/log/ais.err
 
 #* set the proper pacman configuration
