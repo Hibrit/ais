c1a7b44c37ae6b5bd8fef1954558aadd2bdae7cc
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 09:51:00 +0300

    started to using my module :P

---
+++ bin/pages/main_page.py
@@ -0,0 +1,6 @@
+from tui import TUI
+
+
+class MainPage(TUI):
+    def __init__(self):
+        super().__init__()
--- bin/tui/__init__.py
+++ bin/tui/__init__.py
@@ -6,6 +6,7 @@ from subprocess import call, check_call, CalledProcessError
 PATH = dirname(realpath(__file__))
 
 # * before doin' anything run this 3 scripts at first for set up
+# ! these scripts are a bit of a hassle for every startup these are slowing the execution
 for script in ['1_update_time.sh', '2_update_mirrors.sh', '3_check_dialog.sh']:
     call([join(PATH, 'scripts', script)])
 
@@ -29,16 +30,9 @@ class TUI:
         self.dialog = Dialog(dialog='dialog')
         self.dialog.set_background_title('my_tui')
 
-    def run(self):
-        if self.dialog.yesno('yes or no') == self.dialog.OK:
-            self.dialog.msgbox(
-                'this is a message (can be used as an alert too)')
+    def default_page(self):
+        self.dialog.msgbox(
+            'TUI system ready to use you only have to inherit this class create an instance than edit and use update method')
 
-            code, tags = self.dialog.checklist('this is a checklist',
-                                               choices=[('check_box 1', '', False),
-                                                        ('check_box 2', '', False),
-                                                        ('check_box 3', '', False)],
-                                               title='checklist title',
-                                               backtitle='checklist backtitle')
-            if code == self.dialog.OK:
-                print(tags)
+    def update(self):
+        self.default_page()
