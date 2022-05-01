4d27ee43d48b692f3a8a37c695f3c441b2299df8
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 10:02:23 +0300

    .

--- bin/pages/main_page.py
+++ bin/pages/main_page.py
@@ -4,3 +4,11 @@ from tui import TUI
 class MainPage(TUI):
     def __init__(self):
         super().__init__(title='Main Page')
+
+    def update(self):
+        code, tag = self.dialog.menu('Options >> ',
+                                     choices=[('(0)', 'Partitioning and mounting'),
+                                              ('(q)', 'quit')])
+
+        if code == self.dialog.OK:
+            print(self.tag)
