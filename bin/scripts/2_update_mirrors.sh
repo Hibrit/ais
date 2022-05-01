97fe14e2290ac894b56f936aef2f940cab974be4
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 07:07:38 +0300

    .

--- bin/scripts/2_update_mirrors.sh
+++ bin/scripts/2_update_mirrors.sh
@@ -8,11 +8,11 @@ source "$PATH/global.sh"
 
 #* check for wget first
 log_start 'WGET'
-pacman -Sy --noconfirm --needed wget 1>> /var/log/ais.out 2>> /var/log/ais.err
+/usr/bin/pacman -Sy --noconfirm --needed wget 1>> /var/log/ais.out 2>> /var/log/ais.err
 
 #* set the proper pacman configuration
 log_start 'PACMAN CONFIG'
-rm /etc/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
+/usr/bin/rm /etc/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
 
 wget --directory-prefix /etc/ https://raw.githubusercontent.com/Hibrit/archlinuxsettings/master/pacman/pacman.conf 1>> /var/log/ais.out 2>> /var/log/ais.err
 
