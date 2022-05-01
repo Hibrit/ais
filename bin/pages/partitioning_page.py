a908edc907b478c57c43e7110cbd6dfd36ecea25
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 11:07:38 +0300

    .

--- bin/pages/partitioning_page.py
+++ bin/pages/partitioning_page.py
@@ -9,11 +9,11 @@ class PartitioningPage(DriverPage):
     def update(self):
         # TODO first inform user with their current disk scheme
         #!get information with lsblk
-        process = Popen(['ls', '-a'], stdout=PIPE, stderr=PIPE)
+        process = Popen('lsblk -f'.split(), stdout=PIPE, stderr=PIPE)
         out, err = process.communicate()
         print(out)
 
-        self.dialog.msgbox(out.decode(encoding='utf-8'))
+        self.dialog.msgbox(out.decode(encoding='utf-8'), width=.8, height=.5)
         # self.dialog.msgbox('congrats you are using a custom new page')
 
         #! second select which disk to use
