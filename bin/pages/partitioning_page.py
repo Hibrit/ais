670b2d09c1a4e2738b4fd4e0ea7dfe62935ba6d8
hibrit <taskin.n.dogan@yandex.com>
2022-05-01 10:56:51 +0300

    .

--- bin/pages/partitioning_page.py
+++ bin/pages/partitioning_page.py
@@ -5,12 +5,12 @@ class PartitioningPage(DriverPage):
     def __init__(self):
         super().__init__(title='Partitioning Page')
 
-    # def update(self):
+    def update(self):
         # TODO first inform user with their current disk scheme
         #!get information with lsblk
+        self.dialog.msgbox('congrats you are using a custom new page')
 
         #! second select which disk to use
         #! give user the options manual or automatic
         #! to manuel partitioning use cfdisk
         #! to automated partitioning use bash scripting more and more
-        # pass
