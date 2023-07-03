250f6ff3a99731f2eddb9626b11258da10029843
Taşkın Nehir Doğan <taskin.n.dogan@yandex.com>
2023-07-03 04:33:22 +0300

    some refactoring

--- 8_visual_studio_code_setup.sh
+++ 8_visual_studio_code_setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-#! after next login depending a pop up will appear 
-#! a notification asking to install vs code if wanted this script will run
+extensions=(
+  "2gua.rainbow-brackets"
+  "aaron-bond.better-comments"
+  "christian-kohler.path-intellisense"
+  "dsznajder.es7-react-js-snippets"
+  "esbenp.prettier-vscode"
+  "formulahendry.auto-close-tag"
+  "formulahendry.auto-rename-tag"
+  "jdinhlife.gruvbox"
+  "ms-python.python"
+  "ms-python.vscode-pylance"
+  "ms-toolsai.jupyter"
+  "ms-toolsai.jupyter-keymap"
+  "ms-toolsai.jupyter-renderers"
+  "oderwat.indent-rainbow"
+  "PKief.material-icon-theme"
+  "GitHub.copilot"
+)
 
-
-#* install custom extensions
-#! I might change some of those extensions
-code --install-extension 2gua.rainbow-brackets
-code --install-extension aaron-bond.better-comments
-code --install-extension christian-kohler.path-intellisense
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension esbenp.prettier-vscode
-code --install-extension formulahendry.auto-close-tag
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension jdinhlife.gruvbox
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-toolsai.jupyter-keymap
-code --install-extension ms-toolsai.jupyter-renderers
-code --install-extension oderwat.indent-rainbow
-code --install-extension PKief.material-icon-theme
-code --install-extension GitHub.copilot
+for ext in "${extensions[@]}"; do
+  code --install-extension $ext
+done
 
 #* set custom keybindings and settings
 wget --directory-prefix /home/$USER/.config/Code/User/ https://github.com/Hibrit/archlinuxsettings/raw/master/vscode/keybindings.json
